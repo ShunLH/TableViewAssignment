9bddc9c0c78042320d4ad116b142d86c19e644ab
ShunLH <shunleihmu.shunlh@gmail.com>
2019-08-09 10:08:44 +0630

    commit

--- TableViewAssignment/FriendListTableViewCell.swift
+++ TableViewAssignment/FriendListTableViewCell.swift
@@ -9,16 +9,49 @@
 import UIKit
 
 class FriendListTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
+	static let identifier = "FriendListTableViewCell"
+	
+	@IBOutlet weak var ivPicture: UIImageView!
+	
+	@IBOutlet weak var lblName: UILabel!
+	
+	@IBOutlet weak var lblAddress: UILabel!
+	
+	@IBOutlet weak var foodieType: UILabel!
+	
+	@IBOutlet weak var uvFollow: UIView!
+	
+	@IBOutlet weak var lblFollow: UILabel!
+	
+	@IBOutlet weak var btnFollow: UIButton!
+	
+	var isFollow : Bool = false
+	
+	override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+		self.selectionStyle = .none
+		btnFollow.layer.borderWidth = 1
+		btnFollow.layer.borderColor = #colorLiteral(red: 0.1333333333, green: 0.7254901961, blue: 0.6666666667, alpha: 1)
+		btnFollow.layer.cornerRadius = 12
+		ivPicture.layer.cornerRadius = 50
+		
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
+	@IBAction func btnFollow(_ sender: Any) {
+		isFollow = !isFollow
+		if isFollow {
+			btnFollow.backgroundColor = #colorLiteral(red: 0.1333333333, green: 0.7254901961, blue: 0.6666666667, alpha: 1)
+			btnFollow.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+			
+		}else {
+			btnFollow.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+			btnFollow.setTitleColor(#colorLiteral(red: 0.1333333333, green: 0.7254901961, blue: 0.6666666667, alpha: 1), for: .normal)
+
+		}
+		
+	}
 }
--- TableViewAssignment/ViewController.swift
+++ TableViewAssignment/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+struct Person {
+	var name : String
+	var address : String
+	var foodieType : String
+	var isFollow : Bool
+	var image : UIImage
+	
+}
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var tableViewFriendList: UITableView!
+	let friendList :[Person]  = [Person.init(name: "Lisanne Viscaal", address: "Edinburgh", foodieType: "Gold Foodies", isFollow: true,image:#imageLiteral(resourceName: "Girl3")),
+								 Person.init(name: "Tsutsui Ichiha", address: "Andries",foodieType:  "Silver Foodies", isFollow: false,image:#imageLiteral(resourceName: "Girl2")),
+								 Person.init(name: "GvozdenBoskovsky", address: "Kiandra Lowe", foodieType: "Gold Foodies", isFollow: true,image:#imageLiteral(resourceName: "Girl1")),
+								 Person.init(name: "Sampson Totton", address: "Maria Trofimova", foodieType: "Silver Foodies", isFollow: false,image:#imageLiteral(resourceName: "Girl4")),
+								 Person.init(name: "Elwin Sharvill", address: "Munich", foodieType: "Gold Foodies", isFollow: false,image:#imageLiteral(resourceName: "Boy"))]
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		tableViewFriendList.dataSource = self
+		let nib = UINib(nibName: FriendListTableViewCell.identifier, bundle: nil)
+		tableViewFriendList.register(nib, forCellReuseIdentifier: FriendListTableViewCell.identifier)
+		tableViewFriendList.contentInset = UIEdgeInsets(top: 2, left: 0, bottom: 14, right: 0)
 	}
 
 
 }
-
+extension ViewController: UITableViewDataSource {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return friendList.count
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: FriendListTableViewCell.identifier, for: indexPath) as! FriendListTableViewCell
+		let person = friendList[indexPath.row]
+		cell.ivPicture.image = person.image
+		cell.lblName.text = person.name
+		cell.foodieType.text = person.foodieType
+		cell.lblAddress.text = person.address
+		
+		return cell
+	}
+	
+	
+}
