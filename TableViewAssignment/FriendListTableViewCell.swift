6ae8064a93daef50d9c0efda9199c907b0014e4f
ShunLH <shunleihmu.shunlh@gmail.com>
2019-08-09 10:03:16 +0630

    commit

---
+++ TableViewAssignment/FriendListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FriendListTableViewCell.swift
+//  TableViewAssignment
+//
+//  Created by AcePlus Admin on 8/7/19.
+//  Copyright © 2019 SLH. All rights reserved.
+//
+
+import UIKit
+
+class FriendListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
